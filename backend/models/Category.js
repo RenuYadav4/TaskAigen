1e786b21e3d07ca1e7241575812e0eec69afe3f4
Renu Yadav <renuya886@gmail.com>
2025-11-21 00:36:43 +0530

    fixed

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const categorySchema = new mongoose.Schema({
     name: {type: String,
         required: true,
-        unique:true
     },
     user:{type: mongoose.Schema.Types.ObjectId, ref:"User",
         required:true}
